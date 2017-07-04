bc371b26cd84edeb54c464e1213e8a4332fc45a3
Christopher Evans <developer@rtocd.com>
2017-07-04 10:45:07 -0600

    Initial Commit

---
+++ NotificationObservers/NotificationObservers.h
@@ -0,0 +1,19 @@
+//
+//  NotificationObservers.h
+//  NotificationObservers
+//
+//  Created by rtocd on 7/4/17.
+//  Copyright © 2017 RTOCD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NotificationObservers.
+FOUNDATION_EXPORT double NotificationObserversVersionNumber;
+
+//! Project version string for NotificationObservers.
+FOUNDATION_EXPORT const unsigned char NotificationObserversVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NotificationObservers/PublicHeader.h>
+
+
